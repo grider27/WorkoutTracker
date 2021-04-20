a60b56ad4d98fdd7c06ecd1f2eca70c344360333
Gary B <grider27@gmail.com>
2021-04-20 19:06:23 -0400

    functioning api route and bug fix for mongo url

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,15 @@
 const router = require("express").Router();
-const Workout = require("../models/workout.js");
+const { Workout } = require('../models');
+var db = require("../models");
+
+router.get("/api/workouts", (req, res) => {
+    db.Workout.find({}).then(Workout => {
+        res.json(Workout)
+    })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+})
 
 
 module.exports = router;
--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,5 @@
 let mongoose = require("mongoose");
-let db = require("../models/workout");
+let db = require("../models");
 
 mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
@@ -124,8 +124,8 @@ let workoutSeed = [
   }
 ];
 
-db.deleteMany({})
-  .then(() => db.collection.insertMany(workoutSeed))
+db.Workout.deleteMany({})
+  .then(() => db.Workout.collection.insertMany(workoutSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);
